cff37063320c71db93aafe8ed2f5d803dc9c14ef
André Henrique da Silva <ahenqs@gmail.com>
2023-11-25 10:37:51 +0100

    Update en-US

--- en-US/scripts/loader.js
+++ en-US/scripts/loader.js
@@ -5,7 +5,8 @@ function loadCommon() {
     var downloadAppHere = "Download the app here";
     var privacyPolicy = "Privacy Policy";
     var about = "About the app";
-    
+    var supportTitle = "Support";
+
     document.getElementById("appTitle").innerHTML = appTitle;
     document.getElementById("appSubtitle").innerHTML = appSubtitle;
     document.getElementById("appTitleBottom").innerHTML = appTitle;
@@ -15,12 +16,14 @@ function loadCommon() {
     document.getElementById("bottomDownloadAppHere").innerHTML = downloadAppHere;
     document.getElementById("privacyPolicy").innerHTML = privacyPolicy;
     document.getElementById("about").innerHTML = about;
+
+    document.getElementById("supportLink").innerHTML = supportTitle;
 }
 
 
 function loadData() {
     loadCommon();
-    
+
     var text1 = "All your trips";
     var subText1 = "All your past and upcoming trips safely saved in one place.";
     var text2 = "The best trip experience";
@@ -33,7 +36,13 @@ function loadData() {
     var subText5 = "Your trips are directly saved into your iCloud so you can have the same trips on all your devices.";
     var text6 = "Stay tuned";
     var subText6 = "More features will be added in the coming weeks. Make sure to download the app <a href=\"\" class=\"tinted\">here</a> and keep it updated.";
-    
+
+    var supportTitle = "Support";
+    var supportThanks = "Hello there, thanks for using and supporting the app!";
+    var supportMessage = "If you have any requests, suggestions, improvements, translation improvements, feel free to mail me on:";
+    var supportSLA = "You should get a response in no less than 72 hours.";
+    var appleDisclaimer = "Apple Weather and  Weather are trademarks of Apple Inc.";
+
     document.getElementById("text1").innerHTML = text1;
     document.getElementById("subText1").innerHTML = subText1;
     document.getElementById("text2").innerHTML = text2;
@@ -46,4 +55,11 @@ function loadData() {
     document.getElementById("subText5").innerHTML = subText5;
     document.getElementById("text6").innerHTML = text6;
     document.getElementById("subText6").innerHTML = subText6;
+
+    document.getElementById("supportTitle").innerHTML = supportTitle;
+    document.getElementById("supportThanks").innerHTML = supportThanks;
+    document.getElementById("supportMessage").innerHTML = supportMessage;
+    document.getElementById("supportSLA").innerHTML = supportSLA;
+    document.getElementById("appleDisclaimer").innerHTML = appleDisclaimer;
+
 }
